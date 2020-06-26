b0389cc84611459e91c5c91cb218c919c2cf7911
rmallow <rmallow@crimson.ua.edu>
2020-06-25 22:35:22 -0500

    initial message router implementation

--- code/messageRouter.py
+++ code/messageRouter.py
@@ -1,9 +1,32 @@
-from multiprocessing import Queue
+import multiprocessing as mp
+import queue
 
+
+"""
+This class works as an intermediate between the blocks and the handlers
+The message router accepts new messages from triggers and sends
+those messages out to all handlers that are subscribed to that message
+"""
 class messageRouter():
     def __init__(self):
-        self.m_messageQueue = Queue()
+        self.m_end = False
+        self.m_messageQueue = mp.Queue()
         self.m_messageSubscriptions = {}
 
-    def broadcast(self):
-        pass
+    #send to message subscriptions
+    def broadcast(self, message):
+        handlerList = self.m_messageSubscriptions.get(message.m_name, [])
+        for handler in handlerList:
+            handler.send(message)
+
+    def send(self, message):
+        self.m_messageQueue.put(message)
+
+    def start(self):
+        while not self.m_end:
+            try:
+                message = self.m_messageQueue.get()
+                if message is not None:
+                    self.broadcast(message)
+            except queue.Empty:
+                pass
