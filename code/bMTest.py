af511377c0cf5e14c6948a24ca59dfb32accf0c0
rmallow <rmallow@crimson.ua.edu>
2020-06-28 22:18:33 -0500

    changed config loc

--- code/bMTest.py
+++ code/bMTest.py
@@ -1,9 +1,9 @@
 from blockManager import blockManager
 import configLoader
 
-configDict = configLoader.getConfigDictFromFile("../config/testConfig.yml")
+configDict = configLoader.getConfigDictFromFile("../config/testBlockConfig.yml")
 
 bM = blockManager(configDict)
 bM.loadBlocks()
 
-print(bM.m_blockList[0].m_pool.m_actions[1].m_childBlock.m_pool.m_actions[0].m_name)
+bM.start()
