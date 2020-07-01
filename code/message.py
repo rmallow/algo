8f3246dc4bf7f4895138f0c50de823ec25cb0f86
rmallow <rmallow@crimson.ua.edu>
2020-07-01 00:22:58 -0500

    implementation of messages and expansion on messageRouter functionality

--- code/message.py
+++ code/message.py
@@ -1,8 +1,14 @@
 class message():
-    COMMAND_TYPE = "COMMAND"
-    TRIGGER_TYPE = "TRIGGER"
-    
-    def __init__(self, messageType, message, source = None):
+    COMMAND_TYPE = 1
+    TRIGGER_TYPE = 2
+
+    COMMAND_START = 101
+    COMMAND_END = 102
+    COMMAND_ABORT = 103
+    COMMAND_RESUME = 104
+
+    def __init__(self, messageType, message, sourceName = None, sourceCode = None):
         self.m_type = messageType
         self.m_message = message
-        self.m_source = source
+        self.m_sourceName = sourceName
+        self.m_sourceCode = sourceCode
--- code/messageRouter.py
+++ code/messageRouter.py
@@ -2,6 +2,7 @@ import multiprocessing as mp
 import queue
 import message
 import handler
+import logging
 
 
 """
@@ -25,9 +26,10 @@ class messageRouter():
     def send(self, message):
         self.m_messageQueue.put(message)
 
+    #this will handle how the message router responds to various commands
     def processCommand(self, message):
-        pass
-
+        self.CMD_DICT.get(message.m_message,cmdNotFound)(self,message)
+        
     def start(self):
         while not self.m_end:
             try:
@@ -41,3 +43,36 @@ class messageRouter():
                         self.processCommand(message)
                     else:
                         self.broadcast(message)
+
+    def cmdNotFound(self, message):
+        logging.warning("unrecognized command")
+        logging.warning(str(message.m_message))
+
+    def cmdStart(self, message):
+        if message.m_sourceCode not in self.m_handlerUpdateDict:
+            self.m_handlerUpdateDict = []
+        else:
+            logging.warning("start cmd on existing update list, code:")
+            logging.warning(str(message.m_sourceCode))
+
+    def cmdEnd(self, message):
+        updateList = self.m_handlerUpdateDict.pop(message.m_sourceCode.None)
+        if updateList is not None:
+            for handler in updateList:
+                handler.update()
+        else:
+            logging.warning("end cmd on not found code:")
+            logging.warning(str(message.m_sourceCode))
+
+    def cmdAbort(self, message):
+        pass
+
+    def cmdResume(self, message):
+        pass
+
+    CMD_DICT = {
+        101: cmdStart,
+        102: cmdEnd,
+        103: cmdAbort,
+        104: cmdResume
+    }
