acf2ddb39915bacb70336e73eb99f3d65c759595
rmallow <rmallow@crimson.ua.edu>
2020-07-15 16:29:57 -0500

    removed unused code, updated ReadMe with future update goals

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # algo
 By Robert Mallow
 
-With support from Steve Thouvenot
+Create data simulation and live stream management algorithims with ease.
+
+Future Updates:
+    Adding py-qt gui
+    Improving multiprocessing approach and proper grouping
+    Adding easy integration for machine learning handling
--- algo/eventFuncs.py
+++ algo/eventFuncs.py
@@ -1,112 +1,21 @@
 import algo.feed as afd
 
-def testFunc(feed, **kwargs):
-    return {"test4":12345, "periodTest": kwargs['period']}
-
-def average(lst): 
-    return sum(lst) / len(lst) 
 
 """
-Description: Returns the Simple Moving Average (SMA) using a passed in period
+Description: Returns the Simple Moving Average (SMA)
 Output:
-    sma: array
+    sma-${col}: array
 """
 
 def sma(feed, **kwargs):
-    colValue = ""
-    try:
-        kwargs['col']
-    except:
-        colValue = 'Close'
-    else:
-        colValue = kwargs['col']
-
-    neededLength = (kwargs['period']) - 1
-    dataLength = len(feed.m_newData)
-    recentData = []    
-    array = []
-    startingIndex = 0
-    #if the old data is too small and the new data is too small
-    if((len(feed.m_data) < (neededLength + dataLength)) or dataLength == 0) and not(dataLength > neededLength + 1):
-        return {"sma": afd.INSUF_DATA}
-    #if the old data is too small but the new data is large enough
-    if(dataLength > neededLength + 1) and (len(feed.m_data) < (neededLength + dataLength)):
-        recentData = feed.m_newData[:neededLength]
-        startingIndex = neededLength 
-        for n in range(neededLength):
-            array.append(afd.INSUF_DATA)
-    else:
-        recentData = feed.m_data[: len(feed.m_data) - dataLength] [-neededLength:]
-    recentColsData = recentData[colValue]
-    recentCols = []
-    for n in recentColsData:
-        recentCols.append(n)
-    recentCols.insert(0, 0)
-    curCols = feed.m_newData[colValue]
-    for n in curCols[startingIndex:]:
-        recentCols = recentCols[1:]
-        recentCols.append(n)
-        avg = average(recentCols)
-        array.append(avg)   
-    return {"sma": array}
+    
+    return {"sma": 0}
 
 """
-Description: Returns the Exponential Moving Average (EMA) using a passed in period
+Description: Returns the Exponential Moving Average (EMA)
 Output:
-    ema: array
+    ema-${col}: array
 """
 def ema(feed, **kwargs):
-    colValue = ""
-    try:
-        kwargs['col']
-    except:
-        colValue = 'Close'
-    else:
-        colValue = kwargs['col']  
-
-    smoothingFactor = 0
-    try:
-        kwargs['smooth']
-    except:
-        smoothingFactor = 2
-    else:
-        smoothingFactor = kwargs['smooth']
-
-    neededLength = (kwargs['period'])
-    multiplier = (smoothingFactor/(neededLength+1))
-    dataLength = len(feed.m_newData)
-    fullLength = len(feed.m_data)
-    recentData = []    
-    array = []
-    startingIndex = 0
-    #if the old data is too small and the new data is too small
-    if((len(feed.m_data) < (neededLength + dataLength)) or dataLength == 0) and not(dataLength > neededLength + 1):
-        return {"ema": afd.INSUF_DATA}
-    #if the old data is too small but the new data is large enough
-    if(dataLength > neededLength + 1) and (len(feed.m_data) < (neededLength + dataLength)):
-        recentData = feed.m_newData[:neededLength]
-        startingIndex = neededLength 
-        for n in range(neededLength):
-            array.append(afd.INSUF_DATA)
-    else:
-        recentData = feed.m_data[: len(feed.m_data) - dataLength] [-neededLength:]
-    recentColsData = recentData[colValue]
-    recentCols = []
-    for n in recentColsData:
-        recentCols.append(n)
-    prevema = ""
-    try:        
-        len(feed.m_calcData)
-    except:
-        prevema = afd.INSUF_DATA
-    else:
-        lastCalcdata = feed.m_calcData.iloc[-1:]
-        prevema = lastCalcdata['ema'].item()
-    if(prevema == afd.INSUF_DATA):
-        prevema = average(recentCols)
-    curCols = feed.m_newData[colValue]
-    for n in curCols[startingIndex:]:
-        ema = (n * multiplier) + (prevema * (1-multiplier))
-        prevema = ema
-        array.append(ema)
-    return {"ema": array}
+    
+    return {"ema": 0}
