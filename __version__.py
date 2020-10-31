6883df54ab7f7dbe36995f8e961f5df5f483512c
rmallow <rmallow@crimson.ua.edu>
2020-10-31 14:14:22 -0500

    big switch to actually using async right, deleting old files

--- __version__.py
+++ __version__.py
@@ -1,3 +1,3 @@
 # pylint: skip-file
-VERSION = (0, 2, 20)
+VERSION = (0, 2, 21)
 __version__ = '.'.join(map(str, VERSION))
--- algo/handlerManagerAsync.py
+++ algo/handlerManagerAsync.py
@@ -8,3 +8,35 @@ import logging
 def _loadCalcFunc(calcFuncConfig):
     return getattr(importlib.import_module(calcFuncConfig['location']),calcFuncConfig['name'])
 
+"""
+manages all handlers and issues update commands
+gets update lists and corresponding source codes for updates from message Router
+
+"""
+class handlerManager():
+    def __init__(self, configDict):
+        self.m_handlerConfig = configDict
+        self.m_handlerList = []
+        self.m_messageSubscriptions = {}
+        self.m_end = False
+
+    def loadHandlers(self):
+        print("---- Handler Manager Loading Blocks ----")
+        for _, config in self.m_handlerConfig.items():
+            self.m_handlerList.append(self._loadHandler(config))
+        print("---- Handler Manager Done Loading ----")
+
+    def _addSubscriptions(self, subscriptions, val):
+        for subscription in subscriptions:
+            lst = self.m_messageSubscriptions.get(subscription, [])
+            lst.append(val)
+            self.m_messageSubscriptions[subscription] = lst
+
+    def _loadHandler(self, config):
+        name = config['name']
+        subscriptions = config['subscriptions']
+        calcFunc = _loadCalcFunc(config['calcFunc'])
+        
+        val = handler(name, calcFunc)
+        self._addSubscriptions(subscriptions, val)
+        return val
